14ca52e384b2598e0eec2711eccb1d6671329eb9
jaykumar1270 <jaykumar1270@gmail.com>
2024-01-20 19:37:12 +0530

    updated api

--- app/main.py
+++ app/main.py
@@ -5,12 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello Dev World"}
-
+    return {"Hello": "World"}
 
 @app.get("/items")
 def read_item():
     return {"Hello:Item"}
-
-
-#uvicorn main:app --reload
