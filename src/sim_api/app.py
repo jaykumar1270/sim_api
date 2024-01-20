a123e3c210918bf5c628212fc2785c47fe6dd6ea
jaykumar1270 <jaykumar1270@gmail.com>
2024-01-20 15:01:36 +0530

    API created

---
+++ src/sim_api/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items")
+def read_item():
+    return {"Hello:Item"}
